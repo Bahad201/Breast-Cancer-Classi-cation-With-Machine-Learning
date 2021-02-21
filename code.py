533dad7cda5f729265ea3c4e8f74bc582a26446c
Bahadır Yazıcı <bahadir.yazici5@gmail.com>
2021-02-22 00:10:44 +0300

    Breast-Cancer-Classification
    some updates and saving visualization of data

--- code.py
+++ code.py
@@ -21,44 +21,45 @@ warnings.filterwarnings('ignore')
 # data read
 data= pd.read_csv('data.csv')
 yol = data
-# data.drop(['Unnamed: 32','id'],inplace = True, axis=1)
+data.drop(['Unnamed: 32','id'],inplace = True, axis=1)
+#0 benign cells 
+#1 malignant cells
+data=data.rename(columns={"diagnosis":"target"})
 
-# data=data.rename(columns={"diagnosis":"target"})
+sns.countplot(data["target"])
+print(data.target.value_counts())
+plt.savefig('plot0.png')
+data["target"]=[1 if i.strip() == "M" else 0 for i in data.target]
 
-# sns.countplot(data["target"])
-# print(data.target.value_counts())
 
-# data["target"]=[1 if i.strip() == "M" else 0 for i in data.target]
-# # 1 kanser hucre
-# # 0 iyi huylu hucre
+print(data.head())
 
-# print(data.head())
+print("Data Shape",data.shape)
 
-# print("Data Shape",data.shape)
+data.info()
 
-# data.info()
+describe=data.describe()
+data=data.iloc[:,0:31]
 
-# describe=data.describe()
-# data=data.iloc[:,0:31]
-
-# %% EDA
+#%% EDA
 
 # Correlation
 
 corr_matrix = data.corr()
-# annot true korelasyondaki sayilari goster demek
 sns.clustermap(corr_matrix,annot=True, fmt='.2f')
 plt.title('correlation between features')
 
 plt.show()
+
 
 threshold=0.50
 filtre =np.abs(corr_matrix["target"])>threshold
 corr_features=corr_matrix.columns[filtre].tolist()
 sns.clustermap(data[corr_features].corr(),annot=True, fmt='.2f')
 plt.title('correlation between features with threshold 0.75')
+plt.tight_layout()
 plt.show()
-
+plt.savefig('plot1.png')
 
 """
 there some correlated features
@@ -79,8 +80,8 @@ Standardization- normalization
 
 """
 # pair plot
-# sns.pairplot(data[corr_features],diag_kind='kde',markers="+",hue="target")
-# plt.show()
+sns.pairplot(data[corr_features],diag_kind='kde',markers="+",hue="target")
+plt.show()
 
 #%%
 y= data.target
@@ -110,7 +111,7 @@ outlier_score["radius"]=radius
 plt.scatter(x.iloc[:,0],x.iloc[:,1],s =1000*radius, edgecolors="r",facecolors="none",label="Outlier Scores")
 plt.legend()
 plt.show()
-
+plt.savefig('plot2.png')
 # drop outliers
 
 x=x.drop(outlier_index)
@@ -192,7 +193,7 @@ def KNN_Best_Params(x_train,x_test,y_train,y_test):
     plt.xlabel("predicted")
     plt.ylabel("true")
     plt.show()
-    
+    plt.savefig('plot3.png')
     group_names = ['True Neg','False Pos','False Neg','True Pos']
     group_counts = ["{0:0.0f}".format(value) for value in
                 cm_test.flatten()]
@@ -206,6 +207,7 @@ def KNN_Best_Params(x_train,x_test,y_train,y_test):
     plt.xlabel("predicted")
     plt.ylabel("true")
     plt.show()
+    plt.savefig('plot4.png')
     return grid
     
 grid = KNN_Best_Params(X_train,X_test,Y_train,Y_test)
@@ -214,67 +216,69 @@ grid = KNN_Best_Params(X_train,X_test,Y_train,Y_test)
 
 #%%
 # # PCA
-# # reduced the data to two dimensions
-# scaler =StandardScaler()
-# x_scaled = scaler.fit_transform(x)
-# pca =PCA(n_components=2)
-# pca.fit(x_scaled)
-# X_reduced_pca =pca.transform(x_scaled)
-# pca_data = pd.DataFrame(X_reduced_pca,columns =["p1","p2"])
-# pca_data["target"]=y 
-# sns.scatterplot(x= "p1", y= "p2",hue= "target",data= pca_data)
-# plt.title("PCA: p1 vs p2")
-# plt.show()
+# reduced the data to two dimensions
+scaler =StandardScaler()
+x_scaled = scaler.fit_transform(x)
+pca =PCA(n_components=2)
+pca.fit(x_scaled)
+X_reduced_pca =pca.transform(x_scaled)
+pca_data = pd.DataFrame(X_reduced_pca,columns =["p1","p2"])
+pca_data["target"]=y
+sns.scatterplot(x= "p1", y= "p2",hue= "target",data= pca_data)
+plt.title("PCA: p1 vs p2")
+plt.show()
 
 
-# X_train_pca,X_test_pca,Y_train_pca,Y_test_pca=train_test_split(X_reduced_pca,
-#                                                                y,test_size=test_size,
-#                                                                random_state=42)
+X_train_pca,X_test_pca,Y_train_pca,Y_test_pca=train_test_split(X_reduced_pca,
+                                                               y,test_size=test_size,
+                                                               random_state=42)
 
-# grid_pca = KNN_Best_Params(X_train_pca,X_test_pca,Y_train_pca,Y_test_pca)
+grid_pca = KNN_Best_Params(X_train_pca,X_test_pca,Y_train_pca,Y_test_pca)
 
-# # visualize
-# cmap_light =ListedColormap(['orange','cornflowerblue'])
-# cmap_bold = ListedColormap(['darkorange','darkblue'])
+# visualize
+cmap_light =ListedColormap(['orange','cornflowerblue'])
+cmap_bold = ListedColormap(['darkorange','darkblue'])
 
-# h = .05 # step size in the mesh
-# X = X_reduced_pca
+h = .05 # step size in the mesh
+X = X_reduced_pca
 
-# x_min , x_max = X[:,0].min()-1,X[:,0].max()+1
-# y_min , y_max = X[:,0].min()-1,X[:,0].max()+1
-# xx,yy = np.meshgrid(np.arange(x_min,x_max,h),np.arange(y_min,y_max,h))
+x_min , x_max = X[:,0].min()-1,X[:,0].max()+1
+y_min , y_max = X[:,0].min()-1,X[:,0].max()+1
+xx,yy = np.meshgrid(np.arange(x_min,x_max,h),np.arange(y_min,y_max,h))
 
-# Z = grid_pca.predict(np.c_[xx.ravel(),yy.ravel()])
+Z = grid_pca.predict(np.c_[xx.ravel(),yy.ravel()])
 
-# # put the result into a color plot
-# Z =Z.reshape(xx.shape)
-# plt.figure()
-# plt.pcolormesh(xx,yy,Z, cmap = cmap_light)
-# # plot also the training  points
-# plt.scatter(X[:,0],X[:,1],c=y,cmap =cmap_bold,edgecolor ='k',s=20)
-# plt.xlim(xx.min(),xx.max())
-# plt.ylim(yy.min(),yy.max())
-# plt.title("%i-Class classification(k = %i, weights ='%s')".format((len(np.unique(y))),
-#                                                                   grid_pca.best_estimator_.n_neighbors,
-#                                                                   grid_pca.best_estimator_.weights))
+# put the result into a color plot
+Z =Z.reshape(xx.shape)
+plt.figure()
+plt.pcolormesh(xx,yy,Z, cmap = cmap_light)
+# plot also the training  points
+plt.scatter(X[:,0],X[:,1],c=y,cmap =cmap_bold,edgecolor ='k',s=20)
+plt.xlim(xx.min(),xx.max())
+plt.ylim(yy.min(),yy.max())
+plt.title("%i-Class classification(k = %i, weights ='%s')".format((len(np.unique(y))),
+                                                                   grid_pca.best_estimator_.n_neighbors,
+                                                                   grid_pca.best_estimator_.weights))
+plt.savefig('plot5.png')
 
 
-# # %%
-# # NCA neighborhood componenet analysis
-# nca =NeighborhoodComponentsAnalysis(n_components=2,random_state=42)
-# nca.fit(x_scaled,y)
-# X_reduced_nca = nca.transform(x_scaled)
-# nca_data = pd.DataFrame(X_reduced_nca,columns = ["p1","p2"])
-# nca_data["target"] = y
-# sns.scatterplot(x ="p1",y="p2",hue = "target",data = nca_data)
-# plt.title("NCA: 0 iyi huylu hücreler 1 kötü huylu hücreler")
-# plt.show()
+#%%
+# NCA neighborhood componenet analysis
+nca =NeighborhoodComponentsAnalysis(n_components=2,random_state=42)
+nca.fit(x_scaled,y)
+X_reduced_nca = nca.transform(x_scaled)
+nca_data = pd.DataFrame(X_reduced_nca,columns = ["p1","p2"])
+nca_data["target"] = y
+sns.scatterplot(x ="p1",y="p2",hue = "target",data = nca_data)
+plt.title("NCA: 0 benign cells 1 malignant cells")
+plt.show()
+plt.savefig('plot6.png')
 
-# X_train_nca,X_test_nca,Y_train_nca,Y_test_nca=train_test_split(X_reduced_nca,
-#                                                                y,test_size=test_size,
-#                                                                random_state=42)
+X_train_nca,X_test_nca,Y_train_nca,Y_test_nca=train_test_split(X_reduced_nca,
+                                                                y,test_size=test_size,
+                                                                random_state=42)
 
-# grid_nca = KNN_Best_Params(X_train_nca,X_test_nca,Y_train_nca,Y_test_nca)
+grid_nca = KNN_Best_Params(X_train_nca,X_test_nca,Y_train_nca,Y_test_nca)
 
 
 
